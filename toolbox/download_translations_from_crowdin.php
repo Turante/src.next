1c3884cc10db3c1ec9bb22c7045ecb1833ed8119
Restyled.io <commits@restyled.io>
2022-07-17 22:20:51 +0000

    Restyled by php-cs-fixer

--- toolbox/download_translations_from_crowdin.php
+++ toolbox/download_translations_from_crowdin.php
@@ -1,6 +1,8 @@
 <?php
+
 // php download_translations_from_crowdin.php <API_KEY>
-function rglob($pattern, $flags = 0) {
+function rglob($pattern, $flags = 0)
+{
     $files = glob($pattern, $flags);
     foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
         $files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
@@ -23,20 +25,19 @@ passthru('rm -rf ../translations_tmp/et');
 passthru('rm -rf ../translations_tmp/no');
 $folders = glob('../translations_tmp/*');
 foreach ($folders as $folder) {
-  if (stripos($folder, 'values-') === false) {
-    $nfolder = str_replace('-', '-r', $folder);
-    rename($folder, str_replace('../translations_tmp/', '../translations_tmp/values-', $nfolder));
-  }
+    if (stripos($folder, 'values-') === false) {
+        $nfolder = str_replace('-', '-r', $folder);
+        rename($folder, str_replace('../translations_tmp/', '../translations_tmp/values-', $nfolder));
+    }
 }
 $files = glob('../translations_tmp/*/*');
-foreach ($files as $file)
-{
-  echo $file . "\n";
-  // we remove unused / outdated translation strings
-  passthru("sed -i '/facebook_app_id/d' " . $file);
-  passthru("sed -i '/fb_login_protocol_scheme/d' " . $file);
-  passthru("sed -i '/homepage_preferences_enable_news_title/d' " . $file);
-  passthru("sed -i '/homepage_preferences_enable_news_summary/d' " . $file);
+foreach ($files as $file) {
+    echo $file . "\n";
+    // we remove unused / outdated translation strings
+    passthru("sed -i '/facebook_app_id/d' " . $file);
+    passthru("sed -i '/fb_login_protocol_scheme/d' " . $file);
+    passthru("sed -i '/homepage_preferences_enable_news_title/d' " . $file);
+    passthru("sed -i '/homepage_preferences_enable_news_summary/d' " . $file);
 }
 passthru('mkdir -p chrome/android/java/res/');
 passthru('cp -rf ../translations_tmp/* chrome/android/java/res/');
